bc536d4af5d4ea43c91674c447bb4ab871a927c2
Simon Johnstone <sij@google.com>
2020-04-12 13:29:04 +0000

    Import bluetemp.

---
+++ bluetemp/precache-manifest.6bb7806e4f3cc87275638c3ea9ce1a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9af134253d33ebbad472993f99fed04",
+    "url": "/bluetemp/index.html"
+  },
+  {
+    "revision": "2c4717291444766c2cf1",
+    "url": "/bluetemp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5dfc6b65fe18132fec3",
+    "url": "/bluetemp/static/js/2.b28e9327.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/bluetemp/static/js/2.b28e9327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c4717291444766c2cf1",
+    "url": "/bluetemp/static/js/main.c5bf7fd2.chunk.js"
+  },
+  {
+    "revision": "e924cf7eb996e9680a34",
+    "url": "/bluetemp/static/js/runtime-main.06d0c39d.js"
+  }
+]);
